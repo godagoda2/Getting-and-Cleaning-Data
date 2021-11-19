e9d18467bbc1bca44aa3b98f202f12a4ccfd0c72
godagoda2 <90544729+godagoda2@users.noreply.github.com>
2021-11-19 19:36:25 +0900

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,11 +1,16 @@
 run_analysis <- function(){
   
   data_X_train <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
+  data_y_train <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt")
+  
   data_X_test <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
+  data_y_test <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
   
   features <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
   
-  merge_X <- rbind(data_X_train,data_X_test)
+  merge_train <- cbind(data_y_train,data_X_train)
+  merge_test <- cbind(data_y_test, data_X_test)
+  merge_X <- rbind(merge_train,merge_test)
   
   colnames(merge_X) <- features[,2]
   
