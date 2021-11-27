89da28c202ac5dc7b2e257cc3bb40c4bce9df958
godagoda2 <90544729+godagoda2@users.noreply.github.com>
2021-11-27 12:05:43 +0900

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -7,19 +7,29 @@ run_analysis <- function(){
   data_y_test <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt")
   
   features <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt")
+  activity <- read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt")
   
-  merge_train <- cbind(data_y_train,data_X_train)
-  merge_test <- cbind(data_y_test, data_X_test)
-  merge_X <- rbind(merge_train,merge_test)
+  colnames(data_X_train) <- features[,2]
+  colnames(data_y_train) <- "feat"
+  colnames(data_X_test) <- features[,2]
+  colnames(data_y_test) <- "feat"
+  colnames(activity) <- c("num","activity_labels")
   
-  colnames(merge_X) <- features[,2]
-  
-  cols <- grep("mean|std",features[,2])
-  cols_sub <- grep("meanFreq",features[,2])
+  cols <- grep("mean|std",features$V2)
+  cols_sub <- grep("meanFreq",features$V2)
   cols <- setdiff(cols,cols_sub)
   
-  data_out <- merge_X[,cols]
+  data_X_train <- data_X_train[,cols]
+  data_X_test <- data_X_test[,cols]
   
-  write.table(data_out,file = "data.txt",row.names = FALSE)
+  data_y_train_m <- merge(data_y_train,activity,by.x = "feat", by.y = "num")
+  data_y_test_m <- merge(data_y_test,activity,by.x = "feat", by.y = "num")
+  
+  merge_train <- cbind(data_y_train_m,data_X_train)
+  merge_test <- cbind(data_y_test_m, data_X_test)
+  
+  merge_X <- rbind(merge_train,merge_test)
+  
+  write.table(merge_X,file = "tidy_data.txt",row.names = FALSE)
   
 }
